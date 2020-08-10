97e8e92e86674bc0d64bf220d9a44d54280f271e
Lucas <lucasdonuts@gmail.com>
2020-08-10 14:01:42 -0500

    I think I'm done

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let size;
 const container = document.getElementById('container');
 const reset = document.querySelector('#reset');
+let color;
 
 function grid(size = 16) {
     for (i = 0; i < size; i++) {
@@ -19,14 +20,45 @@ function grid(size = 16) {
     }
 };
 
+function randomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return color = 'rgb(' + red + ', ' + blue + ', ' + green + ')';
+}
+
+function darken(target) {
+    let brightness = target.getAttribute('title');
+    target.style.filter = `brightness(${brightness * 10}%)`;
+    if (brightness > 0) {
+        brightness -= 1;
+    }
+    target.setAttribute('title', brightness);
+}
+
 function colorChange(color = 'blue') {
     const hover = container.addEventListener('mouseover', function(e){
-        e.target.style.background = color;
+        if(!e.target.classList.contains('filled')) {
+            e.target.style.background = randomColor();
+            e.target.classList.add('filled');
+            e.target.setAttribute('title', '9');
+        }
+        else {
+            let target = e.target;
+            darken(target);
+        }
         console.log(e);
     })
+};
+
+function resetGrid(e) {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
 }
 
 reset.addEventListener('click', function(e) {
+    resetGrid();
     size = prompt('What size would you like your grid?');
     grid(size);
 })
